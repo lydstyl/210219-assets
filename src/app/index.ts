81236c187725cd0eedcdc5a3e5f68c9a174382ca
Gabriel Brun <lydstyl@gmail.com>
2021-02-19 19:50:08 +0100

    accessSpreadsheet()

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,9 +2,12 @@
 
 require('dotenv').config()
 
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const creds = require('../../client_secret.json')
+
 import { removeNullValues, toFetchingObjects, toPromises } from './exchange'
 
-const exchanges = ['bitstamp', 'bittrex', 'poloniex', 'binance']
+const exchanges = ['bitstamp', 'bittrex', 'poloniex']
 
 function toBalance(exchanges) {
   Promise.all(exchanges).then((balances: any) => {
@@ -24,3 +27,22 @@ function fetchAll(exchanges) {
 }
 
 fetchAll(exchanges)
+
+async function accessSpreadsheet() {
+  const doc = new GoogleSpreadsheet(
+    '1QerSxiIVrG5h8hlJT94OaMeo1KJfNlJQOgsDEsF-MIg',
+  )
+
+  console.log('🚀 ~ accessSpreadsheet ~ creds', creds)
+
+  try {
+    await doc.useServiceAccountAuth(creds)
+
+    await doc.loadInfo() // loads document properties and worksheets
+    console.log(doc.title)
+  } catch (error) {
+    console.log('🚀 ~ accessSpreadsheet ~ error', error.message)
+  }
+}
+
+accessSpreadsheet()
